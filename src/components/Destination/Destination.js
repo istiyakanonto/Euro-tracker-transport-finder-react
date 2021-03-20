bd353ae4bfd9c5f51fe02f10dfff3e3c22bee47f
Istiyak Ahmed <istiyak.anonto@gmail.com>
2021-03-20 21:33:13 +0600

    update

--- src/components/Destination/Destination.js
+++ src/components/Destination/Destination.js
@@ -1,11 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import data from "../../Fakedata/data.json";
+import "./Destination.css";
 
+// import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
 const Destination = () => {
-    return (
+  const { id } = useParams();
+  const selectVehicle = data.find((vehicle) => vehicle.id == id);
+  const [transport, setTransport] = useState([]);
+  useEffect(() => {
+    setTransport(selectVehicle);
+  }, []);
+
+  const { name, photo, location, cost } = transport;
+
+  return (
+    <div>
+      <div className="container card-style">
         <div>
-            <h1>Destination is working......</h1>
+          <img src={photo} alt="" />
+          <p style={{ color: "brown", fontWeight: "600", marginLeft: "80px" }}>
+            {cost}$
+          </p>
         </div>
-    );
+
+        <div>
+          <img src={photo} alt="" />
+          <p style={{ color: "brown", fontWeight: "600", marginLeft: "80px" }}>
+            {cost}$
+          </p>
+        </div>
+
+        <div>
+          <img src={photo} alt="" />
+          <p style={{ color: "brown", fontWeight: "600", marginLeft: "80px" }}>
+            {cost}$
+          </p>
+        </div>
+      </div>
+
+      {/* <Map google={this.props.google} zoom={14}>
+        <Marker onClick={this.onMarkerClick} name={"Current location"} />
+
+        <InfoWindow onClose={this.onInfoWindowClose}>
+          <div>
+            <h1>{this.state.selectedPlace.name}</h1>
+          </div>
+        </InfoWindow>
+      </Map> */}
+    </div>
+  );
 };
+// export default GoogleApiWrapper({
+//   apiKey: "AIzaSyDgQhiQQiEoErSzGvSwznqMMix_jHydXr8",
+// })(Destination);
 
 export default Destination;
